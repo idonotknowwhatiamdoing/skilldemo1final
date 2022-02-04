6a3144641d0a4738bc064bbf4a7e9436f39f9f42
idonotknowwhatiamdoing <50209503+idonotknowwhatiamdoing@users.noreply.github.com>
2022-02-04 04:06:53 -0800

    commit files

---
+++ SkillDemo.java
@@ -0,0 +1,6 @@
+public class SkillDemo {
+    public static int subtract(int x, int y)
+    {
+        return x - y;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+ 
+public class SkillDemoTest {
+    @Test
+    public void subtract() {
+        assertEquals(2, SkillDemo.subtract(5, 3));
+    }
+}
